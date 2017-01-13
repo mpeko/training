ebc853cab307cea90f86cd76cb72e66695e51019
Masa Miya <naturaldining@hotmail.com>
2017-01-13 11:48:44 +0900

    10 PHPでデータベースに保存しているデータをJSON 形式にして返す

--- issues10.php
+++ issues10.php
@@ -1,39 +1,16 @@
 <?php
 
-// PDOクラスのオブジェクトの作成
-$dsn = 'mysql:dbname=training;host=localhost';
-$user = 'testuser';
-$password = 'wXJ00QFPtW51rhdK';
+  // PDOクラスのオブジェクトの作成
+  $dsn = 'mysql:dbname=training;host=localhost';
+  $user = 'testuser';
+  $password = 'wXJ00QFPtW51rhdK';
 
-mb_language("uni");             //カレントの言語を設定
-mb_internal_encoding("utf-8");  //内部文字コードを変更
-mb_http_input("auto");          //HTTP入力文字エンコーディングを検出した結果を返す
-mb_http_output("utf-8");        //HTTP 出力文字エンコーディングを設定
+  $dbh = new PDO($dsn, $user, $password);
 
-try{
-    $dbh = new PDO($dsn, $user, $password);
+  $sql = 'select * from samples';
 
-    $sth = $dbh->prepare('select * from samples');
-    $sth->execute();
-
-    $userData = array();
-
-    while($row = $sth->fetch(PDO::FETCH_ASSOC)){
-        $userData[]=array(
-        'id'=>$row['id'],
-        'name'=>$row['name']
-        );
-    }
-
-    //jsonとして出力
-    header('Content-type: application/json');
-    echo json_encode($userData);
-    
-}catch (PDOException $e){
-    print('Error:'.$e->getMessage());
-    die();
-}
-
-$dbh = null;
+  //jsonとして出力
+  header('Content-type: application/json');
+  echo json_encode($dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC));
 
 ?>
